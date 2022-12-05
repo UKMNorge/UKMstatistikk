a794ed8302ca39be1f363cc1eab74051c751b10b
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-12-05 20:17:27 +0100

    Nytt API kall for antall brukere på delta

---
+++ ajax/getTotalBrukereDelta.ajax.php
@@ -0,0 +1,34 @@
+<?php
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Database\SQL\Query;
+
+// Det brukes POST fordi WP tillater POST bare
+$handleCall = new HandleAPICall([], [], ['GET', 'POST'], false);
+
+// $feedbacks = new Feedbacks();
+
+$retArray = [];
+$hourOrDay = $timelineName == 'day' ? 'hour' : 'day';
+
+$SQL = new Query(
+    "SELECT count(id) as antall from ukm_user WHERE last_login != 'NULL'",
+    [],
+    'ukmdelta'
+);
+
+$SQL2 = new Query(
+    "SELECT count(id) as antall from ukm_user",
+    [],
+    'ukmdelta'
+);
+
+
+$antall_aldri_brukt = $SQL->run();
+$antall = $SQL2->run();
+
+$retArray['antall_ikke_brukt'] = intval(Query::fetch($antall_aldri_brukt)['antall']);
+$retArray['antall'] = intval(Query::fetch($antall)['antall']);
+
+
+// Returner til klienten
+$handleCall->sendToClient($retArray);
