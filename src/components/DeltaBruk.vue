b2c7f8254eeb0f7d1f082656d4133d9a29698c60
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-12-05 16:43:02 +0100

    Delta brukere statistikk fungerer delvis
    Velg av forskjellige tidsrom fungerer ikke ennå

--- src/components/DeltaBruk.vue
+++ src/components/DeltaBruk.vue
@@ -2,9 +2,13 @@
 <template>
     <div>
         <div v-if="initialized">
+            <div class="filter-buttons">
+                <button @click="changeTimeline('day')" :class="{'correct-button' : selectedButton == 'day'}" class="ukm-botton-style">I dag</button>
+                <button @click="changeTimeline('week')" :class="{'correct-button' : selectedButton == 'week'}" class="ukm-botton-style">Siste uke</button>
+                <button @click="changeTimeline('month')" :class="{'correct-button' : selectedButton == 'month'}" class="ukm-botton-style">Siste måned</button>
+            </div>
             <div>
-                <button>Uke</button>
-                <p>290 brukere</p>
+                <canvas id="deltaBrukereChart" style="width:100%;max-width:600px"></canvas>
             </div>
         </div>
     </div>
@@ -14,6 +18,12 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import TabInterface from '../interfaces/tabInterface'
+import { SPAInteraction } from 'ukm-spa/SPAInteraction';
+import { Chart } from 'chart.js';
+import DeltaDate from '../objects/DeltaDate';
+
+type TimelineButton = "day" | "week" | "month";
+
 
 @Component
 export default class DeltaBrukKomponent extends Vue implements TabInterface {
@@ -22,19 +32,78 @@ export default class DeltaBrukKomponent extends Vue implements TabInterface {
 
     enthusiasm = this.initialEnthusiasm;
     public initialized : boolean = false;
+    private spaInteraction = new SPAInteraction(null, 'https://ukm.dev/2023-deatnu-tana-deatnu-tana-sorelv/wp-admin/');
+    public deltaDates : DeltaDate[] = [];
+    public selectedButton : TimelineButton = 'day';
 
     // Opprett nettsiden
     init() : void {
         // Get data via ajax
         this.initialized = true;
+        this.fetchData();
+
     }
 
-    myMethod() : void {
+    public changeTimeline(selectedButton : TimelineButton) {
+        this.selectedButton = selectedButton;
+        this.fetchData();
+    }
+
+    public async fetchData() {
+        var data = {
+            action: 'UKMstatistikk_ajax',
+            subaction: 'getBrukereDelta',
+            from_date: '2020-01-01 12:15:00'
+        };
+
+        var response = await this.spaInteraction.runAjaxCall('admin-ajax.php/', 'POST', data);
+        this.deltaDates = [];
+        for(var $r of response) {
+            this.deltaDates.push(new DeltaDate($r['date'], $r['total']))
+        }
         
+        this.genererChart();
+        
+        return response;
+    }
+
+    genererChart() : void {
+        var labels : string[] = [];
+        var antal : number[] = [];
+
+        for(var d of this.deltaDates) {
+            labels.push(d.getDateString())
+            antal.push(d.getTotal());
+        }
+
+
+        new Chart("deltaBrukereChart", {
+        type: "line",
+        data: {
+            labels: labels,
+            datasets: [{
+            fill: true,
+            lineTension: 0,
+            backgroundColor: "rgba(0,0,255,.1)",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: antal
+            }]
+        },
+        options: {
+            legend: {display: false},
+            scales: {
+            yAxes: [{ticks: {min: 6, max:16}}],
+            }
+        }
+        });
     }
 }
 </script>
 
 <style>
-
+.filter-buttons {
+    padding: 10px;
+    padding-bottom: 30px;
+    padding-top: 20px;
+}
 </style>
---
+++ src/objects/DeltaDate.ts
@@ -0,0 +1,32 @@
+// Representerer antall brukere i en dato
+
+import TableItemInterface from "../interfaces/TableItemInterface";
+
+export default class DeltaDate implements TableItemInterface {
+    private date : Date;
+    private total : number;
+    
+    constructor(date : string, total : number) {
+        this.date = new Date(date);
+        this.total = total;
+    }
+
+    public getDate() : Date {
+        return this.date;
+    }
+
+    public getTotal() : number {
+        return this.total;
+    }
+
+    public getDateString() : string {
+        return this.date.getDate() + '.' + this.date.getMonth() + '.' + this.date.getFullYear();
+    }
+
+    public getKeysForTable() : {navn : string, method : string}[] {
+        return [
+            {navn : 'Dato', method :'getDate'},
+            {navn : 'Antall', method : 'getTotal'},
+        ];
+    }
+}
