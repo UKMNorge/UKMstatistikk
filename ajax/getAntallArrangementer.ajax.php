ae8db3620e1979103855a7b4a6f01a80f27b5258
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-12-16 17:22:51 +0100

    Hent alle arrangementer i kommune og fylke

---
+++ ajax/getAntallArrangementer.ajax.php
@@ -0,0 +1,40 @@
+<?php
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Database\SQL\Query;
+
+// Det brukes POST fordi WP tillater POST bare
+$handleCall = new HandleAPICall([], [], ['GET', 'POST'], false);
+
+$retArray = [];
+
+// Antall per definert år
+$SQL_antall = new Query(
+    "SELECT count(pl_id) as antall from smartukm_place",
+    [],
+);
+$antall = $SQL_antall->run();
+
+// Kommune
+$SQL_antall_kommune = new Query(
+    "SELECT count(pl_id) as antall from smartukm_place WHERE pl_type='kommune'",
+    [],
+);
+$antall_kommune = $SQL_antall_kommune->run();
+
+// Fylke
+$SQL_antall_fylke = new Query(
+    "SELECT count(pl_id) as antall from smartukm_place WHERE pl_type='fylke'",
+    [],
+);
+$antall_fylke = $SQL_antall_fylke->run();
+
+
+$retArray['antall'] = intval(Query::fetch($antall)['antall']);
+$retArray['antall_kommune'] = intval(Query::fetch($antall_kommune)['antall']);
+$retArray['antall_fylke'] = intval(Query::fetch($antall_fylke)['antall']);
+
+// Returner til klienten
+$handleCall->sendToClient($retArray);
+
+
+
